e1c24d40ff83aa9fd6404b975e876bcdb83d67fc
CalledChristian <calledchristian79@gmail.com>
2025-09-20 10:06:58 -0500

    entities update

--- src/main/java/com/example/aplicacionconsumoderest_api_countries/Entity/Country.java
+++ src/main/java/com/example/aplicacionconsumoderest_api_countries/Entity/Country.java
@@ -24,4 +24,76 @@ public class Country {
     private List<Integer> latlng;
     private int area;
     private int population;
+
+    public Name getName() {
+        return name;
+    }
+
+    public void setName(Name name) {
+        this.name = name;
+    }
+
+    public List<String> getCapital() {
+        return capital;
+    }
+
+    public void setCapital(List<String> capital) {
+        this.capital = capital;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getSubregion() {
+        return subregion;
+    }
+
+    public void setSubregion(String subregion) {
+        this.subregion = subregion;
+    }
+
+    public Map<String, Currency> getCurrencies() {
+        return currencies;
+    }
+
+    public void setCurrencies(Map<String, Currency> currencies) {
+        this.currencies = currencies;
+    }
+
+    public Map<String, String> getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(Map<String, String> languages) {
+        this.languages = languages;
+    }
+
+    public List<Integer> getLatlng() {
+        return latlng;
+    }
+
+    public void setLatlng(List<Integer> latlng) {
+        this.latlng = latlng;
+    }
+
+    public int getArea() {
+        return area;
+    }
+
+    public void setArea(int area) {
+        this.area = area;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
 }
